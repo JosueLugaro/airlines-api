f683b45e5b618aebf73ba2d0c43c0fa7092b3617
JosueLugaro <josuelugaro15@gmail.com>
2024-02-16 17:34:33 -0600

    Added query to aircraft endpoint

--- src/aircrafts/aircrafts.cpp
+++ src/aircrafts/aircrafts.cpp
@@ -10,10 +10,18 @@ std::shared_ptr<http_response> aircrafts::Aircrafts::render_GET(
     const http_request &) {
   try {
     char *connection_string = getenv("AIRLINES_CONNECTION");
-    connection *c = new connection(connection_string);
+    connection c(connection_string);
+
+    work w(c);
+    result res = w.exec("SELECT * FROM aircrafts_data");
+    w.commit();
+
+    // TODO: Add conversion from result set to JSON
+
+    return std::shared_ptr<http_response>(
+        new string_response("Aircrafts endpoint"));
   } catch (const std::exception &e) {
     std::cout << e.what() << std::endl;
-  }
-  return std::shared_ptr<http_response>(
-      new string_response("Testing aircrafts endpoint"));
+    return std::shared_ptr<http_response>(new string_response(e.what()));
+  };
 }
