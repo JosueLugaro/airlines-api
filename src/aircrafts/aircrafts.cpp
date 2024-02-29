bf29596b01affbd10721f1cbd3cc5ee5945b941e
JosueLugaro <josuelugaro15@gmail.com>
2024-02-29 15:05:42 -0600

    Fleshed out JsonResponse implementation

--- src/aircrafts/aircrafts.cpp
+++ src/aircrafts/aircrafts.cpp
@@ -1,8 +1,10 @@
-#include "aircrafts.hpp"
+#include "aircrafts/aircrafts.hpp"
+#include "json_response/json_response.hpp"
 
 #include <iostream>
 #include <pqxx/pqxx>
 
+using String = std::string;
 using namespace httpserver;
 using namespace pqxx;
 
@@ -16,10 +18,12 @@ std::shared_ptr<http_response> Aircrafts::render_GET(const http_request &) {
     result res = w.exec("SELECT * FROM aircrafts_data");
     w.commit();
 
-    // TODO: Add conversion from result set to JSON
+    json::JsonResponse<String, String, int> json_response(res);
+
+    std::cout << json_response.get_result_string() << std::endl;
 
     return std::shared_ptr<http_response>(
-        new string_response("Aircrafts endpoint"));
+        new string_response(json_response.get_result_string()));
   } catch (const std::exception &e) {
     std::cout << e.what() << std::endl;
     return std::shared_ptr<http_response>(new string_response(e.what()));
--- src/json_response/json_response.hpp
+++ src/json_response/json_response.hpp
@@ -10,18 +10,46 @@ class JsonResponse {
   std::string result_string;
 
  public:
-  void serialize(const char* key, const int& value);
+  JsonResponse(pqxx::result& result_set) {
+    result_string.append("{");
+    open_data_block();
+    serialize_result_set(result_set);
+    close_data_block();
+    result_string.append("}");
+  };
 
-  void serialize(const char* key, const std::string& value);
+  std::string get_result_string() { return result_string; }
 
-  void serialize_row(pqxx::row& row, pqxx::result& result_set){
-      // WIP
+  void open_data_block() { result_string.append("\"data\": ["); };
+
+  void close_data_block() { result_string.append("]"); }
+
+  void serialize(const char* key, const int& value) {
+    result_string = result_string + "\"" + key + "\"" + ":" + std::to_string(value);
+  };
+
+  void serialize(const char* key, const std::string& value) {
+    result_string = result_string + "\"" + key + "\"" + ":" + "\"" + value + "\"";
+  };
+
+  template <std::size_t I = 0>
+  void serialize_row(std::tuple<TYPES...>& row, pqxx::result& result_set) {
+    if constexpr (I < sizeof...(TYPES)){
+      serialize(result_set.column_name(I), std::get<I>(row));
+      if (I != sizeof...(TYPES) - 1) {
+        result_string.append(",");
+      }
+      serialize_row<I + 1>(row, result_set);
+    }
   };
 
   void serialize_result_set(pqxx::result& result_set) {
     for (auto row : result_set.iter<TYPES...>()) {
+      result_string.append("{");
       serialize_row(row, result_set);
+      result_string.append("},");
     }
+    result_string.pop_back();
   };
 };
 }  // namespace json
